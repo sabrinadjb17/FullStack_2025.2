09bf606e402a18a9389ab3a891f4f903925111e6
sabrinadjb17 <sabrinadjb17@gmail.com>
2025-09-11 17:13:18 -0300

    aula 05 e aitv.

---
+++ aula_5/script.js
@@ -0,0 +1,33 @@
+//window.alert("olá mundo") duas barras, comentário
+console.log("olá mundo") //print no console
+nome = prompt("Qual o seu nome?") //prompt é igual input do pyhton
+console.log("Olá " + nome)
+
+a = 1;
+b = 2;
+
+if(a < b){
+    //
+}
+
+else if(b < a){
+    //
+}
+
+else{
+    //
+}
+
+i = 0;
+while(i < 10){
+    console.log(i) //não é obrigatório colocar ;
+    i++ //= (i = i + 1)
+    // i = +=1k
+}
+
+//exercicio 01
+i = 1
+while(i < 100){
+    console.log(i)
+    i += 2
+}
